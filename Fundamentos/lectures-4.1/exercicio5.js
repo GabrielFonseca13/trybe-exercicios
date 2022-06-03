077f9051145713313288bdd3223de92dc38524be
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-06-03 19:13:48 -0300

    exercicios revisados

--- Fundamentos/lectures-4.1/exercicio5.js
+++ Fundamentos/lectures-4.1/exercicio5.js
@@ -6,3 +6,8 @@ if (bebida === 'cafezinho' && comida === 'pão na chapa') {
 } else {
   console.log('Acho que houve um engano com meu pedido');
 }
+
+// console.log(true && true); // true
+// console.log(true && false); // false
+// console.log(false && true); // false
+// console.log(false && false); // false
---
+++ Fundamentos/lectures-4.1/exercicio8.js
@@ -0,0 +1,8 @@
+const bebidaPrincipal =  'cafezinho';
+const bebidaAlternativa = 'suco de laranja';
+
+if (bebidaPrincipal === 'cafezinho' || bebidaAlternativa === 'suco de laranja') {
+  console.log("Obrigado por me atender :D")
+} else {
+  console.log("Ei, eu não pedi isso!");
+}
