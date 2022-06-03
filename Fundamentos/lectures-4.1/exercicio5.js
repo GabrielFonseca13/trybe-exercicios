89201a0e6b7c309c61be1aee24bc42e474948386
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-06-03 18:52:25 -0300

    exercicio 5

---
+++ Fundamentos/lectures-4.1/exercicio5.js
@@ -0,0 +1,8 @@
+const comida = 'pão na chapa';
+const bebida = 'cafezinho';
+
+if (bebida === 'cafezinho' && comida === 'pão na chapa') {
+  console.log('Muito obrigado pela refeição :)');
+} else {
+  console.log('Acho que houve um engano com meu pedido');
+}
