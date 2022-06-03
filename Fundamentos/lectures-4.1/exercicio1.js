82709258fd0869672431f53f00fe7e716f061159
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-06-03 18:18:02 -0300

    execução dos exercicios 1 e 2

---
+++ Fundamentos/lectures-4.1/exercicio1.js
@@ -0,0 +1,13 @@
+const myName = "Gabriel Fonseca";
+
+const birthCity = "Belo Horizonte";
+// birthCity = "Los Angeles";
+
+let birthYear = 1993;
+birthYear = 2030;
+
+console.log(myName);
+
+console.log(birthCity);
+
+console.log(birthYear);
