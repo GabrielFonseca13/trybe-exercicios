7fb463fce3979e1889a31c6486a3e18536127bd0
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-06-05 15:19:08 -0300

    finaliza exercicio 10

---
+++ Fundamentos/lectures-4.1/exercicio10.js
@@ -0,0 +1,19 @@
+let statusCandidato = "";
+    switch (statusCandidato){
+        case "aprovada":
+        console.log("Aprovada");
+        break;
+        
+        case "lista":
+       console.log("Lista")
+        break;
+
+        case "reprovada":
+        console.log("Reprovada")
+        break;
+
+        default:
+        console.log("não se aplica")
+        break;        
+    }
+
