0560769f94cc7767e91db4fe7fc4b72903f2a28e
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-06-05 20:15:45 -0300

    resolução do exercicio 9

---
+++ Fundamentos/lectures-4.1/exercícios Course/exercicio9.js
@@ -0,0 +1,9 @@
+const number1 = 8;
+const number2 = 10;
+const number3 = 21;
+
+if (number1 % 2 !== 0 || number2 % 2 !== 0 || number3 % 2 !== 0){
+    console.log(true);
+} else {
+    console.log(false);
+}
