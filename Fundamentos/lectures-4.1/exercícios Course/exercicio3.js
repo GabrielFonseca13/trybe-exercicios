56c28e5964547f37344fa7750cb2eb59b165daca
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-06-05 19:11:26 -0300

    resolução do exercicio 3

---
+++ Fundamentos/lectures-4.1/exercícios Course/exercicio3.js
@@ -0,0 +1,11 @@
+const a = 25;
+const b = 30;
+const c = 35;
+
+if (a > b && a > c){
+    console.log('O maior número é '+ a +' (A)');
+}else if (b > a && b > c){
+    console.log('O maior número é '+ b +' (B)');
+}else {
+    console.log('O maior número é '+ c +' (C)');
+}
