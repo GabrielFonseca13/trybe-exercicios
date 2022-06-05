89134b1d85a4e8afd86c5ffdf7f1f9e1f86b4d8b
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-06-05 18:56:28 -0300

    arquivos realocados e resolução do exercicio 1

---
+++ Fundamentos/lectures-4.1/exercícios Course/exercicio1.js
@@ -0,0 +1,9 @@
+const a = 10;
+const b = 5;
+
+console.log("Adição " + (a + b));
+console.log('Subtraçã0 '+ (a - b));
+console.log('Multiplicação'+ (a * b));
+console.log('Divisão '+ (a / b));
+console.log('Módulo '+ (a % b));
+
