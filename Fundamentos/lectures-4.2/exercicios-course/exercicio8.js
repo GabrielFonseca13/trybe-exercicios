ba02511d4170abea8b417b979b98688d55c0c272
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-06-07 23:10:48 -0300

    exercicio 8 pendente

--- Fundamentos/lectures-4.2/exercicios-course/exercicio8.js
+++ Fundamentos/lectures-4.2/exercicios-course/exercicio8.js
@@ -1,5 +1,6 @@
 let elemento = [];
     for(let index = 1; index <= 25 ; index += 1){
-        elemento.push([index])
-    }
-console.log(elemento);
+        elemento.push(index);
+        console.log(elemento[index] / 2);
+}
+    
