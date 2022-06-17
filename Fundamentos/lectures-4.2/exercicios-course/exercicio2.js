67c59c9079d446029027324380391d8f5930f18b
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-06-17 20:34:24 -0300

    resolução exercicios 2 e 3

--- Fundamentos/lectures-4.2/exercicios-course/exercicio2.js
+++ Fundamentos/lectures-4.2/exercicios-course/exercicio2.js
@@ -1,7 +1,6 @@
-
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let somatorium = 0;    
-for(let index = 0; index < numbers.length; index +=1){
-       somatorium = (somatorium + (numbers[index])) 
-}
-console.log(somatorium);
+let sum = 0
+       for(let index = 0; index < numbers.length; index += 1){
+       sum = sum + numbers[index];
+       }
+console.log(sum);
