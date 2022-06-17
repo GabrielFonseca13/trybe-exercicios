ce8d759b5a294e5fade2069d825fa37b3b9d5f58
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-06-17 20:55:21 -0300

    resolução exercicio 7

--- Fundamentos/lectures-4.2/exercicios-course/exercicio7.js
+++ Fundamentos/lectures-4.2/exercicios-course/exercicio7.js
@@ -1,8 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let lowerNumber = numbers[0];
+let menorNumero = numbers[0]
     for(let index = 1; index < numbers.length; index += 1){
-        if(numbers[index] < lowerNumber){
-            lowerNumber = numbers[index]
+        if (numbers[index] < menorNumero){
+            menorNumero = numbers[index];
         }
     }
-console.log(lowerNumber);
+console.log(menorNumero);
