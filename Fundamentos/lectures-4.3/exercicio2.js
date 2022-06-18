804c35db354443230435cdfcab0aafd47af39eae
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-06-18 13:38:13 -0300

    resolução exercicio 4

--- Fundamentos/lectures-4.3/exercicio2.js
+++ Fundamentos/lectures-4.3/exercicio2.js
@@ -5,3 +5,11 @@ for (let index = 0; index < word.length; index += 1){
     reverseWord += word[word.length - 1 - index]
 }
 console.log(reverseWord);
+
+
+//outra solução possível
+
+// let word = 'tryber';
+// let reverseWord = '';
+// reverseWord = word.split('').reverse().join('');
+// console.log(reverseWord);
