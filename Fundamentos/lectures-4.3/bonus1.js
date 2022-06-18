c8e442dd3759d6686d7ae44c6475b7d941d362f2
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-06-18 13:41:03 -0300

    resoluçaõ bonus1

---
+++ Fundamentos/lectures-4.3/bonus1.js
@@ -0,0 +1,10 @@
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  inputLine = inputLine + symbol;
+};
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  console.log(inputLine);
+};
