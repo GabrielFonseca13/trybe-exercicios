730017aae5cfaf4c711d44937f78d05465fe608d
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-06-19 12:39:58 -0300

    correção no exercicio maior número

--- Fundamentos/lectures-4.4/funcaoMaiorNum.js
+++ Fundamentos/lectures-4.4/funcaoMaiorNum.js
@@ -1,8 +1,10 @@
 function maiorNum (primeiroNum, segundoNum){
     if (primeiroNum > segundoNum){
         return 'o primeiro número recebido: '+ primeiroNum + ' é maior que o segundo número recebido: '+ segundoNum +'.';
-    } else {
+    } else if (primeiroNum < segundoNum) {
         return 'o segundo número recebido: '+ segundoNum + ' é maior que o primeiro número recebido: '+ primeiroNum +'.';
+    } else {
+        return 'Os números são iguais.'
     }
 }
-console.log(maiorNum(12 , 4));
+console.log(maiorNum(12 , 12));
