986adc34195f5474b297a5967a6b51140fe8b498
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-06-19 12:02:29 -0300

    exercicio carro para função

--- Fundamentos/lectures-4.4/exercicio2.js
+++ Fundamentos/lectures-4.4/exercicio2.js
@@ -1,7 +1,7 @@
 let car = {
     model: 'A3 Sedan',
     manufacturer: 'Audi',
-    year: '2020',
+    year: 2020,
 };
 for (let key in car){
     console.log(key, car[key]);
---
+++ Fundamentos/lectures-4.4/exercicioCarroFuncao.js
@@ -0,0 +1,25 @@
+let statusCarro = 'desligado';
+let aceleracao = 0;
+
+function ligarDesligar (){
+    if (statusCarro === 'desligado'){
+        statusCarro = 'ligado';
+    } else {
+        statusCarro = 'desligado';
+    }
+    return statusCarro;
+};
+
+function acelerar(incremento){
+    aceleracao = aceleracao + incremento;
+
+    return 'Acelerando a '+ aceleracao + 'Km/h.';
+};
+
+function frear(decremento){
+    aceleracao = aceleracao - decremento;
+
+    return 'Desacelerando a ' + aceleracao + 'Km/h';
+};
+
+ligarDesligar();
