0ee374262b87a599b7067bda458018d2bc00aa05
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-06-30 21:33:27 -0300

    resolução exercicios revisao aula 4.1

---
+++ Fundamentos/lectures-4.1/review13.js
@@ -0,0 +1,11 @@
+// 🚀 Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false.
+
+const a = 0;
+const b = 3;
+const c = 5;
+
+if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0){
+    console.log(true);
+} else {
+    console.log(false);
+}
