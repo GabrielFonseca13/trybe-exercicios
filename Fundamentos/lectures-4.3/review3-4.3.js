c671c4b84e5a3c70f392f36f2e6601f4ca3d4e8a
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-07-07 22:14:33 -0300

    review exercice3 lecture 4.3

--- Fundamentos/lectures-4.3/review3-4.3.js
+++ Fundamentos/lectures-4.3/review3-4.3.js
@@ -1,10 +1,9 @@
 let array = ['java', 'javascript', 'python', 'html', 'css'];
-    for (let index = 0; index < array.length; index+= 1) {
-      if (array[index].length > array[index + 1].length) {
-        console.log(array[index].length)
-        console.log(array[index]);
-        biggestWord = array[index]
+let biggestWord = array[0];
+
+    for (let index = 1; index < array.length; index+=1 ) {
+        if(biggestWord.length < array[index].length){
+            biggestWord = array [index];
+        }
         console.log(biggestWord);
-      }
-        
     }
