907fe8e32483bebf5e901fda77726a0900305d60
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-07-13 20:22:04 -0300

    fim revisao1 lecture 4.4

--- Fundamentos/lectures-4.4/review/review1.js
+++ Fundamentos/lectures-4.4/review/review1.js
@@ -18,3 +18,7 @@ player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018]
 console.log('A jogadora '+ player.name + ' '+ player.lastName+' foi eleita melhor do mundo por '+ player.bestInTheWorld.length+ ' vezes' );
 
 console.log('A jogadora '+ player['name'] + ' '+ player['lastName']+' foi eleita melhor do mundo por '+ player['bestInTheWorld'].length +' vezes' );
+
+console.log('A jogadora possui '+ player.medals.golden + ' medalhas de Ouro e '+ player.medals.silver+ ' medalhas de prata' );
+
+console.log('A jogadora possui '+ player['medals']['golden'] + ' medalhas de Ouro e '+ player['medals']['silver']+ ' medalhas de prata' );
