7ba519760df5b6365939045daa36167ca65e9b9a
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-07-13 20:16:30 -0300

    review 1 aula 4.4

---
+++ Fundamentos/lectures-4.4/review/review1.js
@@ -0,0 +1,20 @@
+let player = {
+  name: "Marta",
+  lastName: "Silva",
+  age: 34,
+  medals: {
+    golden: 2,
+    silver: 3,
+  },
+};
+
+console.log("a jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade.");
+
+console.log("a jogadora " + player['name'] + " " + player['lastName'] + " tem " + player['age'] + " anos de idade.");
+
+
+player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018]
+
+console.log('A jogadora '+ player.name + ' '+ player.lastName+' foi eleita melhor do mundo por '+ player.bestInTheWorld.length+ ' vezes' );
+
+console.log('A jogadora '+ player['name'] + ' '+ player['lastName']+' foi eleita melhor do mundo por '+ player['bestInTheWorld'].length +' vezes' );
