6d974528a1280045f2091ea6c19b6474e7758cef
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-07-13 21:21:54 -0300

    review 13 lecture 4.4

---
+++ Fundamentos/lectures-4.4/review/review13.js
@@ -0,0 +1,8 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  console.log('Bem-vinda, '+ info.personagem);
+  console.log('Bem-vinda, '+ info['personagem']);
