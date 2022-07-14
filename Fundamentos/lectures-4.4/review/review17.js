35d05ff788e4c9b47a8534d7216bf042da9f8ba8
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-07-13 21:48:42 -0300

    review 19 lecture 4.4

--- Fundamentos/lectures-4.4/review/review17.js
+++ Fundamentos/lectures-4.4/review/review17.js
@@ -16,7 +16,7 @@ let info = [
     console.log(info[0].personagem + ' e '+ info[1].personagem);
     console.log(info[0].origem + ' e '+ info[1].origem);
     console.log(info[0].nota + ' e '+ info[1].nota);
-    if (info[0].recorrente +++ info[1].recorrente ){
+    if (info[0].recorrente === info[1].recorrente ){
         console.log('Ambos recorrentes');
     } else {
         console.log('não são recorrente');
