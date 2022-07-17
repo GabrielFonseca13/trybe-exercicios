d7dca6bda05380679565591d58ac62551f99632e
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-07-17 13:33:39 -0300

    resolução ex1 lecture 5.1

---
+++ Fundamentos/lectures-5.1/test.js
@@ -0,0 +1,5 @@
+document.getElementById("page-title").innerText = 'Avengers EndGame'
+
+document.getElementById("second-paragraph").innerText = ' Foi a melhor experiência dentro de um cinema que já vivi e minha vida. O cinema parecia um estádio de futebol.'
+
+document.getElementById("subtitle").innerText = 'Cinema ou estádio de Futebol?'
