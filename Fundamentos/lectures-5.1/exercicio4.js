b451cc7e5897ea7cea2343aa09951172625ab0ca
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-07-18 16:36:35 -0300

    resolução exercicio 2 lecture 5.1

--- Fundamentos/lectures-5.1/exercicio4.js
+++ Fundamentos/lectures-5.1/exercicio4.js
@@ -1,2 +1,5 @@
 const twoYears = document.getElementById('twoYearsAhead');
 twoYears.innerText = 'Casado, sendo um desenvolvedor com grandes habilidades e morando fora do Brasil se possível';
+
+const bgMain = document.querySelectorAll('.main-content')[0];
+bgMain.style.background = 'rgb(76,164,109)';
