e2cf8fe75751df43eb83cdecb1d520faffea4074
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-07-23 18:51:34 -0300

    alteração do texto de cada div com o input

--- Fundamentos/lectures-5.3/script.js
+++ Fundamentos/lectures-5.3/script.js
@@ -27,7 +27,10 @@ thirdLi.addEventListener('click', addRemoveTech);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
-
+input.addEventListener('input', function(event){
+    const techElement = document.querySelector('.tech');
+    techElement.innerText = event.target.value
+});
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
