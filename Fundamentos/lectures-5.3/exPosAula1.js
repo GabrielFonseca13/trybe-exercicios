0da37621f7a7fef59896e24ef0d0d5758e59ac9c
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-07-23 21:42:11 -0300

    criando um botao dianmicamente e colocando a classe.

--- Fundamentos/lectures-5.3/exPosAula1.js
+++ Fundamentos/lectures-5.3/exPosAula1.js
@@ -15,6 +15,15 @@ function createDaysOfTheWeek() {
   
   // Escreva seu código abaixo.
 
+//   Exercício 1:
+// Crie um calendário dinamicamente.
+// O array decemberDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Sua função deve criar dinamicamente cada dia do calendário e os adicionar dentro da tag <ul>.
+// Note que os dias 29 e 30 de novembro estão no array, pois representam respectivamente Domingo e Segunda-feira.
+// A tag <ul> deve conter o id 'days';
+// Os dias devem estar contidos em uma tag <li>, e todos devem ter a classe day. Ex: <li class="day">3</li>
+// Os dias 24, 25 e 31 são feriados e, além da classe day, devem conter também a classe holiday. Ex: <li class="day holiday">24</li>
+// Os dias 4, 11, 18 e 25 são sextas-feiras. Eles devem conter a classe day e a classe friday. Ex: <li class="day friday">4</li>
+ 
   let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
   function createDaysOfTheMonth() {
@@ -25,7 +34,6 @@ function createDaysOfTheWeek() {
       let day = decemberDaysList[index];
       let dayItem = document.createElement('li');
       dayItem.innerHTML = day;
-      // getDaysList.appendChild(dayItem);
 
       if (day === 24 || day === 31){
         dayItem.className = 'day holiday';
@@ -43,3 +51,22 @@ function createDaysOfTheWeek() {
     }
   }
     createDaysOfTheMonth();
+
+//     🚀 Exercício 2:
+// Implemente uma função que crie dinamicamente um botão com o nome "Feriados".
+// Sua função deve receber um parâmetro com a string 'Feriados'
+// Adicione a este botão a ID "btn-holiday"
+// Adicione este botão como filho/filha da tag <div> com classe "buttons-container"
+
+const btnContainner = document.querySelector('.buttons-container');
+
+function createHolidayButton(buttonName){
+    let newButton = document.createElement('button');
+    newButton.id = 'btn-holiday';
+    
+    newButton.innerHTML = buttonName;
+        
+    btnContainner.appendChild(newButton);
+}
+
+createHolidayButton('Feriados');
