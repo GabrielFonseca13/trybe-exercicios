2c238d16d77d16016468e4e103f11d09269d3ca6
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-07-25 18:04:25 -0300

    exemplos de utilização do sessionStorage.

--- Fundamentos/lectures-5.4/script.js
+++ Fundamentos/lectures-5.4/script.js
@@ -1,3 +1,5 @@
+// exemplos de utilização do localStorage
+
 document.cookie = 'email=isabella@email.com';
 console.log(localStorage.length); // não possui nada salvo, então o retorno é o valor: 0
 localStorage.setItem('firstname', 'Adam'); // salva uma entrada com a key = 'firstname' e value = 'Adam'
@@ -8,3 +10,16 @@ localStorage.removeItem('lastname'); // remove a entrada referente a key = 'last
 console.log(localStorage.length); // possui uma entrada, então o retorno é o valor: 1
 localStorage.clear(); // limpa todas as entradas salvas em localStorage
 console.log(localStorage.length); // não possui nada salvo, então o retorno é o valor: 0
+
+
+// exemplos de utilização do sessionStorage.
+
+console.log(sessionStorage.length); // não possui nada salvo, então o retorno é o valor: 0
+sessionStorage.setItem('firstname', 'Adam'); // salva uma entrada com a key = 'firstname' e value = 'Adam'
+sessionStorage.setItem('lastname', 'Smith'); // salva uma entrada com a key = 'lastname' e value = 'Smith'
+console.log(sessionStorage.getItem('lastname')); // retorna o valor 'Smith'
+console.log(sessionStorage.length); // possui duas entradas, então o retorno é o valor: 2
+sessionStorage.removeItem('lastname'); // remove a entrada referente a key = 'lastname'
+console.log(sessionStorage.length); // possui uma entrada, então o retorno é o valor: 1
+sessionStorage.clear(); // limpa todas as entradas salvas em sessionStorage
+console.log(sessionStorage.length); // não possui nada salvo, então o retorno é o valor: 0
