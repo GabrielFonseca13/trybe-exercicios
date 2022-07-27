a8c6f7b8c86e8de034e4f8bd8106674ee153990d
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-07-27 19:03:13 -0300

    função para alterar background e cor do texto

--- Fundamentos/lectures-5.4/exercicio1.js
+++ Fundamentos/lectures-5.4/exercicio1.js
@@ -1,1 +1,20 @@
-console.log('teste');
+window.onload = function(){
+
+    let select = document.getElementById('selectBG')
+    console.log(select);
+    select.addEventListener('change', function(){
+        let selectedColor = select.selectedOptions[0];
+        console.log(select.selectedOptions);
+        document.body.style.backgroundColor = selectedColor.value;
+    })
+
+    let text = document.getElementById('text');
+    let selectTextColor = document.getElementById('selectTextColor');
+    selectTextColor.addEventListener('change', function(){
+        let selectedTextColor = selectTextColor.selectedOptions[0];
+        console.log(selectedTextColor);
+        text.style.color = selectedTextColor.value;
+    })
+
+    let fontSize = document.getElementById('fontSize');
+}
