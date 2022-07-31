2e69b90923b6cdbb2e442539e991f0f2004a69e7
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-07-31 19:42:05 -0300

    Utilizando o template Literals

--- Fundamentos/lectures-7.1/testes7-1.js
+++ Fundamentos/lectures-7.1/testes7-1.js
@@ -7,10 +7,15 @@
 //   userInfo();
 
 
-if (true) {
-    // inicio do escopo do if
-    var userAge = '20';
-    console.log(userAge); // 20
-    // fim do escopo do if
-  }
-  console.log(userAge); // 20
+// if (true) {
+//     // inicio do escopo do if
+//     var userAge = '20';
+//     console.log(userAge); // 20
+//     // fim do escopo do if
+//   }
+//   console.log(userAge); // 20
+
+// Exemplos template literals
+
+const myName = 'Gabriel Dev';
+console.log(`Este é o Primeiro Template Literals de ${myName} !!`);
