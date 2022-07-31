f7240c7273cf0713b8a900652eb506346b43f57a
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-07-31 19:22:41 -0300

    exemplo 2 do uso de var let e const

--- Fundamentos/lectures-7.1/testes7-1.js
+++ Fundamentos/lectures-7.1/testes7-1.js
@@ -1,7 +1,16 @@
-function userInfo() {
-    let userEmail = 'maria@email.com';
+// function userInfo() {
+//     let userEmail = 'maria@email.com';
   
-    // Toda expressão dentro do escopo da função userInfo tem acesso à variável userEmail
-    console.log(userEmail);
+//     // Toda expressão dentro do escopo da função userInfo tem acesso à variável userEmail
+//     console.log(userEmail);
+//   }
+//   userInfo();
+
+
+if (true) {
+    // inicio do escopo do if
+    var userAge = '20';
+    console.log(userAge); // 20
+    // fim do escopo do if
   }
-  userInfo();
+  console.log(userAge); // 20
