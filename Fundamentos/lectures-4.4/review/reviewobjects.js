45e9de0be4bd892665ad10b9f91a502c55167ebe
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-07-30 21:15:15 -0300

    construindo o desafio 2

--- Fundamentos/lectures-4.4/review/reviewobjects.js
+++ Fundamentos/lectures-4.4/review/reviewobjects.js
@@ -10,4 +10,25 @@ function calculaAreaEPerimetro(base, altura){
     return objeto;
 }
 
-console.log(calculaAreaEPerimetro(5,7));
+console.log(calculaAreaEPerimetro(5,7)); 
+
+// let numeros = {
+//     pares = 0,
+//     impares = 0,
+// }
+
+function informaParImpar(array){
+    let contPar = 0;
+    let contImpar = 0;
+    for (let index = 0; index < array.length; index +=1){
+        if (array[index] % 2 === 0){
+            contPar += 1;
+        } else {
+            contImpar += 1;
+        }
+        return contImpar, contPar;
+    }
+    console.log('o array tem '+ contPar +' números pares');
+    console.log('o array tem '+ contImpar +' números Impares');
+}
+console.log(informaParImpar([5,25,37,17,18,3,10]));
