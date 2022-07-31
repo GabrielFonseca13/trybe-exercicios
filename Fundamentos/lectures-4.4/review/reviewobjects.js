6697b48127884ed30933504d681f0c1b4280a8ea
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-07-31 16:10:56 -0300

    criação do objeto e do array

--- Fundamentos/lectures-4.4/review/reviewobjects.js
+++ Fundamentos/lectures-4.4/review/reviewobjects.js
@@ -1,4 +1,4 @@
-// EXERCICIO 1 - Crie uma função que calculoe a área e o perímetro de im quadrilátero qualquer.
+// EXERCICIO 1 - Crie uma função que calcule a área e o perímetro de um quadrilátero qualquer.
 // Essa função deve receber dois parametros: base e altura e retornar um objeto no formato:
 // Fórmulas: perímetro = (2* base) + (2*altura) area = base * altura.
 
@@ -13,31 +13,23 @@ function calculaAreaEPerimetro(base, altura){
 
     return objeto;
 }
-
-console.log(calculaAreaEPerimetro(5,7)); 
+console.log(objeto);
+console.log(calculaAreaEPerimetro(5,7));
+console.log(objeto);
 
 //EXERCICIO 2 - Crie um função que, dado im array de números inteiros, 
 // retorne a quantidade de números pares e impares no formato.
-
 
 let numeros = {
     pares: 0,
     impares: 0,
 }
 
-function informaParImpar(numeros){
-    for (let index = 0; index < numeros.length; index +=1){
-        if (numeros[index] % 2 === 0){
-            numeros['pares'] += 1;
-        } else {
-            numeros['impar'] += 1;
-        }
-        return numeros['par'], numeros['impar'];
+let arrayParaTeste = [];
+    for (let i = 0; i <= 20; i +=1 ){
+        arrayParaTeste.push(i);
     }
-    console.log('o array tem '+ numeros['par'] +' números pares');
-    console.log('o array tem '+ numeros['impar'] +' números Impares');
-}
-console.log(informaParImpar([5,25,37,17,18,3,10]));
+    console.log(arrayParaTeste);
 
 // EXERCICIO3 - Crie uma função que receba uma string ending. Verifique se a string ending é o final da string word.
 // Considere que a string ending sempre será menor que a string world.
