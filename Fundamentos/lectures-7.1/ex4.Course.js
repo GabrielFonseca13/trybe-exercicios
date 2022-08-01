1c4b6e37ee1a18cf76974e0793a487aa54033e16
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-01 19:21:49 -0300

    resolução em uma linha

--- Fundamentos/lectures-7.1/ex4.Course.js
+++ Fundamentos/lectures-7.1/ex4.Course.js
@@ -4,16 +4,22 @@
 const longestWord = (text) => {
     const textArray = text.split(" ");
     let biggestWord = textArray[0];
-    console.log(textArray);
-    console.log(biggestWord);
+    // console.log(textArray);
+    // console.log(biggestWord);
     for (let index = 1; index < textArray.length; index +=1) {
         if (textArray[index].length > biggestWord.length) {
             biggestWord = textArray[index];            
         };
-        console.log(biggestWord);       
+        // console.log(biggestWord);       
     }
     return biggestWord;
 }
 
-longestWord('Antônio foi no banheiro e não sabemos o que aconteceu') // retorna 'aconteceu'
+console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu')); // retorna 'aconteceu'
+
+
+// alternativa para reduzir todo o código acima.
+// const longestWord = (text) => text.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0];
+
+// console.log(longestWord('Antonio foi ao banheiro e não sabemos o que aconteceu'));
 
