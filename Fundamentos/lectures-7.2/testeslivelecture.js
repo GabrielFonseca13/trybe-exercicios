c065d9d7f428a1232e3b7f2e6d6e82a0693c9454
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-05 17:11:25 -0300

    usando o object.assing para criar um novo objeto, copia de um objeto original.

--- Fundamentos/lectures-7.2/testeslivelecture.js
+++ Fundamentos/lectures-7.2/testeslivelecture.js
@@ -12,5 +12,15 @@ const person = {
 // const keys = Object.keys(person);
 // console.log(keys);
 
-console.log(Object.keys(person).includes('fly'));
+// // console.log(Object.keys(person).includes('fly'));
+// // console.log(Object.values(person));
+// console.log(Object.entries(person)[0][0]);
+// console.log(Object.entries(person)[1][1]);
 
+// const person2 = person; 
+// console.log('PESSOA 1 ' + person, ' CÓPIA PESSOA 1 ' + person2);
+
+const person2 = Object.assign({}, person);
+console.log(person, person2);
+person.age = 12;
+console.log(person, person2);
