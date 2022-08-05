5a61ecbbe8226a33de330883e008be6c1608d79b
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-04 21:46:51 -0300

    exercicio para uso do Objectentries

---
+++ Fundamentos/lectures-7.2/objectEntries.js
@@ -0,0 +1,11 @@
+const coolestTvShow = {
+    name: "BoJack Horseman",
+    genre: "adult animation",
+    createdBy: "Raphael Bob-Waksberg",
+    favoriteCharacter: "Princess Carolyn",
+    quote: "Princess Carolyn always lands on her feet.",
+    seasons: 6,
+  };
+  
+  console.log(Object.entries(coolestTvShow));
+  
