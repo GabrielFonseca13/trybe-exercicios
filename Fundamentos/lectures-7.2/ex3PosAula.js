87504630ca0090c5c1fd39be0747262a3053b0d9
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-05 19:29:31 -0300

    criando um objeto com outros 3 objetos com o Object.Assign

--- Fundamentos/lectures-7.2/ex3PosAula.js
+++ Fundamentos/lectures-7.2/ex3PosAula.js
@@ -25,13 +25,21 @@ criaTurnoNoite(lesson2, "turno", "noite");
 console.log(lesson2);
 
 // Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+
 const listaKeys = (objeto) => console.log(Object.keys(objeto));
 listaKeys(lesson2);
 
 // Crie uma função para mostrar o tamanho de um objeto. Essa função deve receber um objeto como parâmetro.
+
 const showLength = objeto => console.log(Object.entries(objeto).length);
 showLength(lesson2);
 
 // Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+
 const listaValues = objeto => console.log(Object.values(objeto));
 listaValues(lesson2);
+
+// Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1, lesson2 e lesson3. Ao executar o comando console.log(allLessons), a saída deverá ser a seguinte:
+
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
+console.log(allLessons);
