f2442487f9bd5a021830a4c9746e9947a3f56cf8
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-05 19:16:00 -0300

    criando função para medir o tamanho do objeto.

--- Fundamentos/lectures-7.2/ex3PosAula.js
+++ Fundamentos/lectures-7.2/ex3PosAula.js
@@ -1,28 +1,33 @@
 const lesson1 = {
-    materia: 'Matemática',
-    numeroEstudantes: 20,
-    professor: 'Maria Clara',
-    turno: 'manhã',
-  };
-  
-  const lesson2 = {
-    materia: 'História',
-    numeroEstudantes: 20,
-    professor: 'Carlos',
-  };
-  
-  const lesson3 = {
-    materia: 'Matemática',
-    numeroEstudantes: 10,
-    professor: 'Maria Clara',
-    turno: 'noite',
-  };
+  materia: "Matemática",
+  numeroEstudantes: 20,
+  professor: "Maria Clara",
+  turno: "manhã",
+};
 
-  const criaTurnoNoite = (objeto, chave, valor) => objeto[chave] = valor;
-  
-  criaTurnoNoite(lesson2, 'turno', 'noite');
+const lesson2 = {
+  materia: "História",
+  numeroEstudantes: 20,
+  professor: "Carlos",
+};
 
-  console.log(lesson2);
+const lesson3 = {
+  materia: "Matemática",
+  numeroEstudantes: 10,
+  professor: "Maria Clara",
+  turno: "noite",
+};
 
-  const listaKeys = objeto => Object.keys(objeto);
-  console.log(listaKeys(lesson2));
+//   Crie uma função para adicionar o turno da noite na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+
+const criaTurnoNoite = (objeto, chave, valor) => (objeto[chave] = valor);
+criaTurnoNoite(lesson2, "turno", "noite");
+console.log(lesson2);
+
+// Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+const listaKeys = (objeto) => console.log(Object.keys(objeto));
+listaKeys(lesson2);
+
+// Crie uma função para mostrar o tamanho de um objeto. Essa função deve receber um objeto como parâmetro.
+const showLength = objeto => console.log(Object.entries(objeto).length);
+showLength(lesson2);
