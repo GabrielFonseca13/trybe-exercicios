44013f3645d1439705c4e8940105abc6d2b416c6
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-07 15:55:10 -0300

    testando o toThrow

---
+++ Fundamentos/lectures-7.3/throw/script.js
@@ -0,0 +1,10 @@
+const multiplyByTwo = (number) => {
+    if (!number) {
+      throw new Error('number é indefinido')
+    }
+    return number * 2;
+  };
+  multiplyByTwo(4);
+
+  module.exports = multiplyByTwo;
+ 
---
+++ Fundamentos/lectures-7.3/throw/script.test.js
@@ -0,0 +1,11 @@
+ const multiplyByTwo = require('./script');
+
+  test('testa se multiplyByTwo retorna o resultado da multiplicação', () => {
+    expect(multiplyByTwo(4)).toBe(8);
+  });
+  test('testa se é lançado um erro quando number é indefinido', () => {
+    expect(() => { multiplyByTwo() }).toThrow();
+  });
+  test('testa se a mensagem de erro é "number é indefinido"', () => {
+    expect(() => { multiplyByTwo() }).toThrowError(new Error('number é indefinido'));
+  });
