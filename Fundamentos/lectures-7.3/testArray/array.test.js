dcfc346febe4103295663498ebe988938ad901e5
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-07 16:56:25 -0300

    testando Describe no Jest

---
+++ Fundamentos/lectures-7.3/testArray/array.test.js
@@ -0,0 +1,21 @@
+const { it } = require('node:test');
+const { describe } = require('yargs');
+const myArray = require('./array')
+
+describe('requisito 1', () => {
+    it('verifica se o array reccebe 1,2,3,4,5 e retorna true', () => {
+        expect(true).toEqual(myArray([1,2,3,4,5])); 
+    });
+});
+
+describe('Requisito 2', () => {
+    it(`verifica se a funçao recebe [1,2,'3',4,5] e retorna false`, () => {
+        expect(false).toEqual(myArray([1,2,'3',4,5]));
+    });
+})
+
+describe('Requisito 3', () => {
+    it (`verifica se a função recebe um [' '] e retorna false`, () => {
+        expect(false).toEqual(myArray([' ']));
+    });
+})
