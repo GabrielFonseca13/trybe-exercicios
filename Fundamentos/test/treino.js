d43411cf9b826713fa3800e73aa020427da78a5d
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-11 20:00:53 -0300

    inserindo exercicio para o treino de funções.

---
+++ Fundamentos/test/treino.js
@@ -0,0 +1,24 @@
+// 1. Imprimir números de 1 a 10 
+
+// 2. Imprimir os números impares menores que 100 
+
+// 3. Imprimir a tabuada do número 8 
+
+// 4. Imprimir todas as tabuadas do número 1 ao 10 
+
+// 5. Soma dos números de 1 a 10 
+
+// 6 Calculado o fatorial de 10 
+
+// 7. Criar uma função para calcular a porcentagem de um número 
+
+// 8. Calcular a soma dos números impares maiores que 10 e menores que 30 
+
+// 9. Criar uma função que converta uma temperatura de Celsius para Fahrenheit 
+
+// 10 . Calcular a soma dos números de um array 
+
+// 11. Calcular a média de todos os números de um array 
+
+// 12. Criar uma função que receba como parâmetro um array de números e retorne um array
+// contendo somente números positivos. 
