d290df3a11426a09bea8f8b75e1cff7f2569e7d6
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-13 20:56:42 -0300

    finalizando o exercicio 3 do dia 8.1 do course, criando uma hof que compara as notas de um aluno com as notas corretas e retorne a nota.

--- Fundamentos/lectures-8.1/ex3.js
+++ Fundamentos/lectures-8.1/ex3.js
@@ -14,3 +14,29 @@
 // Uma resposta incorreta reduz a pontuação final em 0.5 ponto.
 
 // Ao final, a HOF deve retornar o total de pontos obtidos através das respostas fornecidas pela pessoa estudante. Utilize os seguintes arrays:
+
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const checkAnswers = (rightAnswers, studentAnswers) => {
+    rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+    studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+    let studentGrade = 0
+    for (let index = 0; index < rightAnswers.length; index += 1){
+        if (rightAnswers[index] === studentAnswers[index]){
+            studentGrade += 1;
+        } else if (rightAnswers[index] !== studentAnswers[index] && studentAnswers[index] === 'N.A'){
+            studentGrade = studentGrade;
+        } else if (rightAnswers[index] !== studentAnswers[index] && studentAnswers[index] !== 'N.A'){
+            studentGrade -= 0.5;
+        }
+    }
+    return studentGrade;
+};
+
+const studentScore = (rightAnswers, studentAnswers, callback) => {
+    rightAnswers = [];
+    studentAnswers = [];
+    return `Resultado Final é: ${callback()} Pontos!`
+}
+console.log(studentScore(rightAnswers, studentAnswers, checkAnswers));
