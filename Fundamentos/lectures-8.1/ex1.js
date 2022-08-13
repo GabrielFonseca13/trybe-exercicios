6b1337e6bd55232893778d4a93323faa236a8ade
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-13 19:56:27 -0300

    passando a função como callback para retornar o objeto com nome completo e email

--- Fundamentos/lectures-8.1/ex1.js
+++ Fundamentos/lectures-8.1/ex1.js
@@ -5,17 +5,17 @@ const employeeGenerator = (nomeCompleto) => {
     return {nomeCompleto, email: `${email}@trybe.com`}
     };
 
-console.log(employeeGenerator('Gabriel Fonseca'));
-
 // Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id. 
 
 // A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com.
 
-const newEmployees = () => {
+const newEmployees = (callback) => {
     const employees = {
-      id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+      id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
     }
     return employees;
   };
+
+  console.log(newEmployees(employeeGenerator));
