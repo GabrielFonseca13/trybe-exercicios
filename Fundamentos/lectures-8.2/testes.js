ded7dfbc2ab79ff1da5c9b2c8aa695c247ae6935
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-14 20:56:23 -0300

    testes do forEach em JS aula 8.2

---
+++ Fundamentos/lectures-8.2/testes.js
@@ -0,0 +1,6 @@
+const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+arrayOfValues.forEach((element, indexOfTheArray, theEntireArray) => {
+  console.log('Cada elemento do array:', element);
+  console.log('Index, posição do elemento:', indexOfTheArray);
+  console.log('Array percorrido:', theEntireArray);
+});
