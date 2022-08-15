cc0b891897223540c79e92726f02465d583f7c18
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-14 21:09:10 -0300

    usando for each para criar tabuadad de 2

--- Fundamentos/lectures-8.2/testes.js
+++ Fundamentos/lectures-8.2/testes.js
@@ -1,6 +1,12 @@
-const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
-arrayOfValues.forEach((element, indexOfTheArray, theEntireArray) => {
-  console.log('Cada elemento do array:', element);
-  console.log('Index, posição do elemento:', indexOfTheArray);
-  console.log('Array percorrido:', theEntireArray);
+// // const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+// arrayOfValues.forEach((element, indexOfTheArray, theEntireArray) => {
+//   console.log('Cada elemento do array:', element);
+//   console.log('Index, posição do elemento:', indexOfTheArray);
+//   console.log('Array percorrido:', theEntireArray);
+// });
+
+let multiplicadores = [1,2,3,4,5,6,7,8,9,10];
+console.log('### TABUADA DE 2 ###')
+multiplicadores.forEach((item) => {
+    console.log(`${item} X 2 é igual a : ${item *2}`) 
 });
