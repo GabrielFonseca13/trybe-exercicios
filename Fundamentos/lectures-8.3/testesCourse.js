50ded1bbaf8f6bc9f3b34dc16197334a0a1d7b00
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-18 12:06:19 -0300

    fazendo os primeiros testes da HOF .filter()

---
+++ Fundamentos/lectures-8.3/testesCourse.js
@@ -0,0 +1,34 @@
+// const arr = ['ana', 'luiz', 'gabriel'];
+
+// arr.filter((element, index, arrayCompleto) => {
+//     console.log(element,index,arrayCompleto);
+// });
+
+const notasAlunos = [
+    {    
+    nome: 'Gabriel',
+    nota: 100
+    },
+    {    
+    nome: 'Micheli',
+    nota: 90
+    },
+    {    
+    nome: 'Daniel',
+    nota: 75
+    },
+    {    
+    nome: 'Luiz',
+    nota: 79
+    },
+    {    
+    Nome: 'Nando',
+    nota: 80
+    }
+];
+
+const alunosAprovados = notasAlunos.filter((element) => element.nota >= 80);
+console.log(alunosAprovados);
+
+const alunosReprovados = notasAlunos.filter((element) => !(element.nota >= 80));
+console.log(alunosReprovados);
