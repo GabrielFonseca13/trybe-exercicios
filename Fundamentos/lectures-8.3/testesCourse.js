dd13ba8de3b314f1d77012e4f11df34c47e01cd3
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-18 12:31:28 -0300

    fazendo os primeiros testes da HOF .filter()

--- Fundamentos/lectures-8.3/testesCourse.js
+++ Fundamentos/lectures-8.3/testesCourse.js
@@ -4,31 +4,46 @@
 //     console.log(element,index,arrayCompleto);
 // });
 
-const notasAlunos = [
-    {    
-    nome: 'Gabriel',
-    nota: 100
-    },
-    {    
-    nome: 'Micheli',
-    nota: 90
-    },
-    {    
-    nome: 'Daniel',
-    nota: 75
-    },
-    {    
-    nome: 'Luiz',
-    nota: 79
-    },
-    {    
-    Nome: 'Nando',
-    nota: 80
-    }
-];
+// const notasAlunos = [
+//     {    
+//     nome: 'Gabriel',
+//     nota: 100
+//     },
+//     {    
+//     nome: 'Micheli',
+//     nota: 90
+//     },
+//     {    
+//     nome: 'Daniel',
+//     nota: 75
+//     },
+//     {    
+//     nome: 'Luiz',
+//     nota: 79
+//     },
+//     {    
+//     Nome: 'Nando',
+//     nota: 80
+//     }
+// ];
 
-const alunosAprovados = notasAlunos.filter((element) => element.nota >= 80);
-console.log(alunosAprovados);
+// const alunosAprovados = notasAlunos.filter((element) => element.nota >= 80);
+// console.log(alunosAprovados);
 
-const alunosReprovados = notasAlunos.filter((element) => !(element.nota >= 80));
-console.log(alunosReprovados);
+// const alunosReprovados = notasAlunos.filter((element) => !(element.nota >= 80));
+// console.log(alunosReprovados);
+
+const objPeople = [
+    { name: 'José', age: 21 },
+    { name: 'Lucas', age: 19 },
+    { name: 'Maria', age: 16 },
+    { name: 'Gilberto', age: 18 },
+    { name: 'Vitor', age: 15 },
+  ];
+
+  const canDrive = (arrayDePessoas) =>
+    arrayDePessoas.filter((pessoa) => pessoa.age >= 18);
+    console.log(canDrive(objPeople));
+   const cantDrive = (arrayDePessoas) => 
+   arrayDePessoas.filter((pessoa) => !(pessoa.age >= 18)); 
+   console.log(cantDrive(objPeople));
