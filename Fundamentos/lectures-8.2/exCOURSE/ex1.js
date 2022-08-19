1a0ea16c58158e75f1417bcfbaa65fe9040f36eb
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-19 12:12:14 -0300

    resolução exercicio 2 dia 8.2

--- Fundamentos/lectures-8.2/exCOURSE/ex1.js
+++ Fundamentos/lectures-8.2/exCOURSE/ex1.js
@@ -1,8 +1,24 @@
 const books = require('./base.js');
 
-const bornAt1947 = (obj, year) => {
-    const buscaAutor = obj.find((element) => element.author.birthYear === year);
-    return buscaAutor.author.name;
-}
+// const bornAt1947 = (array, year) => {
+//     const buscaAutor = array.find((element) => element.author.birthYear === year);
+//     return buscaAutor.author.name;
+// }
 
-console.log(bornAt1947(books, 1947));
+// console.log(bornAt1947(books, 1947));
+
+const smallerName = (array) => {
+    const arrayNomeLivros = [];
+    const buscaLivrosObjeto = array.forEach((element)=> {
+    arrayNomeLivros.push(element.name);
+    });
+
+    let menorLivro = arrayNomeLivros[0];
+    for (let index = 1; index < arrayNomeLivros.length; index+=1){
+        if (arrayNomeLivros[index].length < menorLivro.length){
+            menorLivro = arrayNomeLivros[index];
+        }
+    };
+    return menorLivro;
+};
+console.log(smallerName(books));
