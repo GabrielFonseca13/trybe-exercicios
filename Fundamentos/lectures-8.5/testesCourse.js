6eb3ac77a9aa25172edff928ea8c3b9962e14c9a
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-25 10:57:50 -0300

    testes spread Opeartor no Array com Objetos

--- Fundamentos/lectures-8.5/testesCourse.js
+++ Fundamentos/lectures-8.5/testesCourse.js
@@ -1,7 +1,27 @@
-const carros = ['Gol','Palio','HB20'];
-const motos = ['Biz', 'Hornet'];
+// const carros = ['Gol','Palio','HB20'];
+// const motos = ['Biz', 'Hornet'];
 
-const veiculos = [...motos, ...carros];
+// const veiculos = [...motos, ...carros];
 
-console.log(veiculos);
+// console.log(veiculos);
 
+const pessoa = {
+    nome: 'Gabriel',
+    idade: 29,
+    cidade: 'Belo Horizonte',
+}
+
+const conhecimentosTrybe = {
+    softSkill: true,
+    hardSkill: true,
+    trabalho: true,
+}
+
+const pessoaTryber = {
+    ...pessoa,
+    ...conhecimentosTrybe,
+    esporte: 'Futevolei',
+    Musica: 'Pagode',
+}
+
+console.log(pessoaTryber);
