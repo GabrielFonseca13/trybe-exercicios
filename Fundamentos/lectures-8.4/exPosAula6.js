e6b1f0f9fe85c3e35df15636989653088eaea23f
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-25 10:31:15 -0300

    exercicios 5 e 6

---
+++ Fundamentos/lectures-8.4/exPosAula6.js
@@ -0,0 +1,14 @@
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+function studentAverage() {
+  return nameAndAverage = students.map((student, index) => (
+    {
+    name: student,
+    average: (grades[index].reduce((acc, curr) => acc + curr, 0) / grades[index].length),
+  }
+  ));
+}
+
+console.log(studentAverage())
+// ...
