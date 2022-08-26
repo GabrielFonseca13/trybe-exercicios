c888cb2324c3beb738c6712afd4e8a2dd92f08e4
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-26 19:17:30 -0300

    testes com parametro rest e reduce

--- Fundamentos/lectures-8.5/testesCourse.js
+++ Fundamentos/lectures-8.5/testesCourse.js
@@ -5,23 +5,27 @@
 
 // console.log(veiculos);
 
-const pessoa = {
-    nome: 'Gabriel',
-    idade: 29,
-    cidade: 'Belo Horizonte',
-}
+// const pessoa = {
+//     nome: 'Gabriel',
+//     idade: 29,
+//     cidade: 'Belo Horizonte',
+// }
 
-const conhecimentosTrybe = {
-    softSkill: true,
-    hardSkill: true,
-    trabalho: true,
-}
+// const conhecimentosTrybe = {
+//     softSkill: true,
+//     hardSkill: true,
+//     trabalho: true,
+// }
 
-const pessoaTryber = {
-    ...pessoa,
-    ...conhecimentosTrybe,
-    esporte: 'Futevolei',
-    Musica: 'Pagode',
-}
+// const pessoaTryber = {
+//     ...pessoa,
+//     ...conhecimentosTrybe,
+//     esporte: 'Futevolei',
+//     Musica: 'Pagode',
+// }
 
-console.log(pessoaTryber);
+// console.log(pessoaTryber);
+
+const sum = ( ...args) => args.reduce((acc, curr)=> acc + curr, 0);
+
+console.log(sum(1,2,3,4,5,6,7,8,9,10));
