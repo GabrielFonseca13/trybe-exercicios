43c0b7fff49ee11685ad407a17ca41ec2ed055b6
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-31 20:15:47 -0300

    primeiros testes de Object Destructuring

--- Fundamentos/lectures-8.5/testesCourse.js
+++ Fundamentos/lectures-8.5/testesCourse.js
@@ -8,7 +8,7 @@
 // const pessoa = {
 //     nome: 'Gabriel',
 //     idade: 29,
-//     cidade: 'Belo Horizonte',  
+//     cidade: 'Belo Horizonte',
 // }
 
 // const conhecimentosTrybe = {
@@ -26,14 +26,22 @@
 
 // console.log(pessoaTryber);
 
-// // const sum = ( ...args) => args.reduce((acc, curr)=> acc + curr, 0);
+// const sum = (...args) => args.reduce((acc, curr) => acc + curr, 0);
 
-// // console.log(sum(1,2,3,4,5,6,7,8,9,10));
+// console.log(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 
-function quantosParams(...args) {
-    console.log('parâmetros:', args);
-    return `Você passou ${args.length} parâmetros para a função.`;
-  }
-  
-  console.log(quantosParams(0, 1, 2)); // Você passou 3 parâmetros para a função.
-  console.log(quantosParams('string', null, [1, 2, 3], { })); // Você passou 4 parâmetros para a função.
+// function quantosParams(...args) {
+//     console.log('parâmetros:', args);
+//     return `Você passou ${args.length} parâmetros para a função.`;
+//   }
+
+//   console.log(quantosParams(0, 1, 2)); // Você passou 3 parâmetros para a função.
+//   console.log(quantosParams('string', null, [1, 2, 3], { })); // Você passou 4 parâmetros para a função.
+
+const product = {
+  name: 'Smart TV Crystal UHD',
+  price: '1899.05',
+  seller: 'Casas de Minas',
+};
+const { name } = product;
+console.log(name); // Smart TV Crystal UHD
