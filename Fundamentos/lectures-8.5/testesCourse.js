4b1faf58d1e04568c352678117dca8ed51aa2ca6
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-31 19:53:55 -0300

    primeiros testes com o parametro rest

--- Fundamentos/lectures-8.5/testesCourse.js
+++ Fundamentos/lectures-8.5/testesCourse.js
@@ -1,14 +1,14 @@
-// const carros = ['Gol','Palio','HB20'];
-// const motos = ['Biz', 'Hornet'];
+// // const carros = ['Gol','Palio','HB20'];
+// // const motos = ['Biz', 'Hornet'];
 
-// const veiculos = [...motos, ...carros];
+// // const veiculos = [...motos, ...carros];
 
-// console.log(veiculos);
+// // console.log(veiculos);
 
 // const pessoa = {
 //     nome: 'Gabriel',
 //     idade: 29,
-//     cidade: 'Belo Horizonte',
+//     cidade: 'Belo Horizonte',  
 // }
 
 // const conhecimentosTrybe = {
@@ -26,6 +26,14 @@
 
 // console.log(pessoaTryber);
 
-const sum = ( ...args) => args.reduce((acc, curr)=> acc + curr, 0);
+// // const sum = ( ...args) => args.reduce((acc, curr)=> acc + curr, 0);
 
-console.log(sum(1,2,3,4,5,6,7,8,9,10));
+// // console.log(sum(1,2,3,4,5,6,7,8,9,10));
+
+function quantosParams(...args) {
+    console.log('parâmetros:', args);
+    return `Você passou ${args.length} parâmetros para a função.`;
+  }
+  
+  console.log(quantosParams(0, 1, 2)); // Você passou 3 parâmetros para a função.
+  console.log(quantosParams('string', null, [1, 2, 3], { })); // Você passou 4 parâmetros para a função.
