f4222b585ec2b94345625c92621102c4a4f88479
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-31 20:22:01 -0300

    exemplo de utilização de sprea operator e do Objetct destructuring

--- Fundamentos/lectures-8.5/testesCourse.js
+++ Fundamentos/lectures-8.5/testesCourse.js
@@ -47,21 +47,34 @@
 // console.log(name); // Smart TV Crystal UHD
 
 // definindo o objeto
-const character = {
-  name: 'Luke SkyWalker',
-  age: '53',
-  description: {
-    specieName: 'Human',
-    jedi: true,
-  },
-  homeWorld: {
-    name: 'Tatooine',
-    population: '200000',
-  },
+// const character = {
+//   name: 'Luke SkyWalker',
+//   age: '53',
+//   description: {
+//     specieName: 'Human',
+//     jedi: true,
+//   },
+//   homeWorld: {
+//     name: 'Tatooine',
+//     population: '200000',
+//   },
+// };
+
+// // desestruturando o objeto:
+// const { name, age, homeWorld: { name: planetName }, description: { jedi } } = character;
+
+// // imprimindo os valores:
+// console.log(`Esse é o ${name}, ele tem ${age} anos, mora no planeta ${planetName} e, por incrível que possa parecer, ele ${jedi ? 'é um Jedi' : 'não é um Jedi'}.`);
+
+
+const daysOfWeek = {
+  workDays: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+  weekend: ['Saturday', 'Sunday'],
 };
 
-// desestruturando o objeto:
-const { name, age, homeWorld: { name: planetName }, description: { jedi } } = character;
+const { workDays, weekend } = daysOfWeek;
+console.log(workDays); // ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+console.log(weekend); // ['Saturday', 'Sunday']
 
-// imprimindo os valores:
-console.log(`Esse é o ${name}, ele tem ${age} anos, mora no planeta ${planetName} e, por incrível que possa parecer, ele ${jedi ? 'é um Jedi' : 'não é um Jedi'}.`);
+const weekdays = [...workDays, ...weekend];
+console.log(weekdays); // ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
