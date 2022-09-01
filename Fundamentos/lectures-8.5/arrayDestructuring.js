7e0f7e509f51b9ce0a5e741044106e8289a8e308
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-08-31 23:17:08 -0300

    exemplos de array destrucutring e 3 exercicios para fixar

---
+++ Fundamentos/lectures-8.5/arrayDestructuring.js
@@ -0,0 +1,7 @@
+const arrayCountries = ['Brazil', 'Japan', 'China', 'Canada'];
+const [firstCountry, secondCountry, thirdCountry, fourthCountry] = arrayCountries;
+
+console.log(firstCountry); // Brazil
+console.log(secondCountry); // Japan
+console.log(thirdCountry); // China
+console.log(fourthCountry); // Canada
