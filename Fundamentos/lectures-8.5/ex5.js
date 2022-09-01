831b717b645791d18fb1970be21481d8467598a9
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-09-01 15:53:45 -0300

    resolução do ex5 com array destructuring

--- Fundamentos/lectures-8.5/ex5.js
+++ Fundamentos/lectures-8.5/ex5.js
@@ -1,6 +1,13 @@
-// 5 - Escreva a função swap, que dado um array de 3 elementos, retorna um novo array com o primeiro e terceiro elementos trocados. Detalhe: você precisa fazer essa função gastando somente 1 linha. O retorno da função utilizando o array myList deve ser [3, 2, 5].
+// 5 - Escreva a função swap, que dado um array de 3 elementos, 
+// retorna um novo array com o primeiro e terceiro elementos trocados. 
+// Detalhe: você precisa fazer essa função gastando somente 1 linha. 
+// O retorno da função utilizando o array myList deve ser [3, 2, 5].
 // Dica: use array destructuring.
 
 const myList = [5, 2, 3];
 
 // escreva swap abaixo
+
+const swap = ([x,y,z]) => [z,y,x];
+
+console.log(swap(myList));
