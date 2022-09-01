9ed9924e5f52d9857f6a0b763a8459ab7a9bd5ab
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-09-01 16:13:06 -0300

    resolução do ex8 com Default Parameters

--- Fundamentos/lectures-8.5/ex8.js
+++ Fundamentos/lectures-8.5/ex8.js
@@ -1,7 +1,13 @@
-// 8 - Escreva uma função greet que, dado o nome de uma pessoa, retorna uma mensagem de cumprimento:
+// 8 - Escreva uma função greet que, dado o nome de uma pessoa, 
+// retorna uma mensagem de cumprimento:
+
 // Dica: use default params.
 
 // escreva greet abaixo
+
+const greet = (nome, saudacao = 'Hi') => {
+    return `${saudacao} ${nome}`;
+}
 
 console.log(greet('John')) // 'Hi John'
 console.log(greet('John', 'Good morning')) // 'Good morning John'
