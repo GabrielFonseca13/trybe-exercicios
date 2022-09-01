35a69c82b975c6dc8f397dcf9eed1bd71701a12d
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-09-01 10:56:45 -0300

    lista de exercicios aula 8.5

---
+++ Fundamentos/lectures-8.5/ex4.js
@@ -0,0 +1,34 @@
+// 🚀 4 - Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+// Dica: use object destructuring.
+
+
+const people = [
+    {
+      name: 'Nicole',
+      bornIn: 1992,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Harry',
+      bornIn: 2008,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Toby',
+      bornIn: 1901,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Frida',
+      bornIn: 1960,
+      nationality: 'Dannish',
+    },
+    {
+      name: 'Fernando',
+      bornIn: 2001,
+      nationality: 'Brazilian',
+    },
+  ];
+  //bornIn: nascido em
+  
+  // escreva filterPeople abaixo
