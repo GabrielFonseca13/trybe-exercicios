8132131deabe892e921f95b23096dd61f3f65bed
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-09-01 00:02:19 -0300

    Utilizando default destructuring com array destructuring

--- Fundamentos/lectures-8.5/defaultDestructuring.js
+++ Fundamentos/lectures-8.5/defaultDestructuring.js
@@ -1,3 +1,5 @@
+//######### DEFAULT DESTRUCTURING ###############
+
 const person = {
   name: 'João',
   lastName: 'Jr',
@@ -10,3 +12,12 @@ const person = {
 
 const { nationality = 'Brazilian' } = person;
 console.log(nationality); // Brazilian
+
+//######### DEFAULT DESTRUCTURING EM CONJUNTO COM ARRAY DESTRUCTURING ###############
+
+const position2d = [1.0, 2.0];
+const [x, y, z = 0] = position2d;
+
+console.log(x); // 1
+console.log(y); // 2
+console.log(z); // 0
