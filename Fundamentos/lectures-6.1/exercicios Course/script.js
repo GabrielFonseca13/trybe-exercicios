18ee2d43028863d4aa42550e52d7ee98f2f4e714
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-09-02 21:51:37 -0300

    resolução ex1

---
+++ Fundamentos/lectures-6.1/exercicios Course/script.js
@@ -0,0 +1,18 @@
+// Nome Completo:
+// Limitado entre 10 e 40 caracteres.
+const fullname = getElementById('full-name')
+ 
+//E-mail:
+// Limitado entre 10 e 50 caracteres.
+ 
+// Destino Preferido:
+// 4 opções: Cidade, Campo, Praia, Montanhas.
+ 
+// Por que você deveria ser a pessoa desenvolvedora a ganhar o concurso TrybeTrip?
+// Limite de 500 caracteres.
+ 
+// Qual a melhor data para realizar sua viagem? 
+
+// Gostaria de receber outras incríveis oportunidades oferecidas pela Trybe?
+
+// Concordo que imagens das minhas férias poderão ser usadas na divulgação de concursos futuros.
