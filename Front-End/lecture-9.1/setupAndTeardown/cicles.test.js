a2a9db1f28b4ebe2fc5300d8d6596fcc2e86b8f1
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-09-08 22:52:26 -0300

    Usando o After Each para limpar o array para cada teste.

--- Front-End/lecture-9.1/setupAndTeardown/cicles.test.js
+++ Front-End/lecture-9.1/setupAndTeardown/cicles.test.js
@@ -34,6 +34,37 @@ const removeCity = (city) => {
 
 // ################### Setup BeforeEach() #######################
 
+// beforeEach(() => {
+//   cities = [...cities, 'Pindamonhangaba'];
+// });
+
+
+// test('Testa a função addCity utilizando o beforeEach', () => {
+//   expect.assertions(5);
+//   addCity('Campinas');
+//   addCity('Goiania');
+//   addCity('Recife');
+//   expect(cities).toHaveLength(4);
+//   expect(cities).toContain('Campinas');
+//   expect(cities).toContain('Goiania');
+//   expect(cities).toContain('Recife');
+//   expect(cities).toContain('Pindamonhangaba');
+// });
+
+// test('Testa a função removeCity utilizando o beforeEach', () => {
+//   expect.assertions(5);
+//   removeCity('Campinas');
+//   expect(cities).toHaveLength(4);
+//   expect(cities).not.toContain('Campinas');
+//   expect(cities).toContain('Goiania');
+//   expect(cities).toContain('Recife');
+//   expect(cities).toContain('Pindamonhangaba');
+//   console.log(cities);
+// });
+
+
+// ######################## TEARDOWN AfterEach() ################################
+
 beforeEach(() => {
   cities = [...cities, 'Pindamonhangaba'];
 });
