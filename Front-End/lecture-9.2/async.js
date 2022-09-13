53dd93307a4cf1e57399119e9af547389aa493fe
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-09-13 00:04:57 -0300

    exemplo de async

---
+++ Front-End/lecture-9.2/async.js
@@ -0,0 +1,14 @@
+const fetch = require('node-fetch');
+
+const fetchJoke = async () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data.value))
+    .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+}
+
+fetchJoke();
+
+// Chuck Norris can write multi-threaded applications with a single thread.
