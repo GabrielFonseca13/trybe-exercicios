89a52bdf47a92c2cfcb317a1b614d8101abd5513
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-09-13 00:28:22 -0300

    exemplos do uso do async, com await, mesclado com.then e .catch, e com try e catch

--- Front-End/lecture-9.2/async.js
+++ Front-End/lecture-9.2/async.js
@@ -1,14 +1,50 @@
+// ####### EXEMPLO ASYNC 1
+
+// const fetch = require('node-fetch');
+
+// const fetchJoke = async () => {
+//   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.value))
+//     .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+// }
+
+// fetchJoke();
+
+
+// ####### EXEMPLO 2 ASYNC COM AWAIT MESCLADO COM .THEN() E .CATCH()
+
+// const fetch = require('node-fetch');
+
+// const fetchJoke = async () => {
+//   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+//   const result = await fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => data.value)
+//     .catch((error) => `Algo deu errado :( \n${error}`);
+
+//   console.log(result);
+// };
+
+// fetchJoke();
+
+// ######## EXEMPLO 3 ASYNC COM TRY E CATCH
+
 const fetch = require('node-fetch');
 
 const fetchJoke = async () => {
   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => console.log(data.value))
-    .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.value);
+  } catch(error) {
+    console.log(`Algo deu errado :( \n${error}`);
+  }
 }
 
 fetchJoke();
-
-// Chuck Norris can write multi-threaded applications with a single thread.
--- Front-End/lecture-9.2/fetch.js
+++ Front-End/lecture-9.2/fetch.js
@@ -38,7 +38,7 @@ const fetchJoke = () => {
 
 fetchJoke();
 
-// Note que para forçar o erro retiramos o https:// do início da url.
+// Note que para forçar o erro retiramos o https:// do início da url. traz o retorno:Assim como o .then() recebe o retorno caso a requisição seja bem sucedida, o .catch() recebe o erro gerado caso a requisição não seja bem sucedida, que é passado para ele como argumento de sua função interna. Assim, quando o fetch retorna algum erro, todos os .then() são pulados e é executado o primeiro .catch() encontrado. E tem mais! O .catch() também "pega" qualquer erro que acontecer dentro de qualquer .then() anterior a ele. Por esse motivo ele é geralmente usado no final.
 // Algo deu errado :(
 // TypeError: Only absolute URLs are supported
 
