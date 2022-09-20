8199596423e11f290c0bb81c0e28b430f4f5acfc
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-09-20 14:47:31 -0300

    Exercicio 1 sobre componentes e props

---
+++ Front-End/Sessão 2 - Introdução ao React/lecture-2.2 - Componentes React/course1/src/App.js
@@ -0,0 +1,13 @@
+import Image from './Image';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Image  source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" />
+    </div>
+  );
+}
+
+export default App;
+
