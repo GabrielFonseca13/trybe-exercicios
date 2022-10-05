ad2dc3883bb35bded4ca0f570729485ffc911ed7
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-10-05 15:07:23 -0300

    exemplo 1 do course - testando o ciclo de vida do react

--- Front-End/Sessão 4 - Ciclo de vida de Componentes e React Router/lecture-4.1 - Ciclo de vida dos componentes/my-interdimensional-app/src/App.js
+++ Front-End/Sessão 4 - Ciclo de vida de Componentes e React Router/lecture-4.1 - Ciclo de vida dos componentes/my-interdimensional-app/src/App.js
@@ -1,3 +1,47 @@
+// EX Course 1 ##############
+import React from "react";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+    console.log("construtor");
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("shouldComponentUpdate");
+    return true;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate");
+  }
+
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <p>Contador</p>
+        <button
+          onClick={() => this.setState((state) => ({ counter: state.counter + 1 }))}
+        >
+          Soma
+        </button>
+        <p>{this.state.counter}</p>
+      </div>
+    );
+  }
+}
+
+export default Counter;
+
+
 // App.js EX 1 ################
 // import React, { Component } from 'react';
 // import './App.css';
@@ -44,69 +88,69 @@
 
 // EX 2 ##########################################
 
-import React from 'react';
-import Joke from './components/Joke';
+// import React from 'react';
+// import Joke from './components/Jokeg';
 
-class DadJoke extends React.Component {
-  constructor() {
-    super();
+// class DadJoke extends React.Component {
+//   constructor() {
+//     super();
 
-    this.saveJoke = this.saveJoke.bind(this);
+//     this.saveJoke = this.saveJoke.bind(this);
 
-    this.state = {
-      jokeObj: undefined,
-      loading: true,
-      storedJokes: [],
-    }
-  }
+//     this.state = {
+//       jokeObj: undefined,
+//       loading: true,
+//       storedJokes: [],
+//     }
+//   }
 
-  async fetchJoke() {
-    this.setState(
-      { loading: true }, // Primeiro parâmetro da setState()!
-      async () => {
-      const requestHeaders = { headers: { Accept: 'application/json' } }
-      const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders)
-      const requestObject = await requestReturn.json();
-      this.setState({
-        loading: false,
-        jokeObj: requestObject
-      });
-    });
-  }
+//   async fetchJoke() {
+//     this.setState(
+//       { loading: true }, // Primeiro parâmetro da setState()!
+//       async () => {
+//       const requestHeaders = { headers: { Accept: 'application/json' } }
+//       const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders)
+//       const requestObject = await requestReturn.json();
+//       this.setState({
+//         loading: false,
+//         jokeObj: requestObject
+//       });
+//     });
+//   }
 
-  componentDidMount() {
-    this.fetchJoke();
-  }
+//   componentDidMount() {
+//     this.fetchJoke();
+//   }
 
-  saveJoke() {
-    this.setState(({ storedJokes, jokeObj }) => ({
-      storedJokes: [...storedJokes, jokeObj]
-    }));
+//   saveJoke() {
+//     this.setState(({ storedJokes, jokeObj }) => ({
+//       storedJokes: [...storedJokes, jokeObj]
+//     }));
 
-    this.fetchJoke();
-  }
+//     this.fetchJoke();
+//   }
 
-  render() {
-    const { storedJokes, loading, jokeObj } = this.state;
-    const loadingElement = <span>Loading...</span>;
+//   render() {
+//     const { storedJokes, loading, jokeObj } = this.state;
+//     const loadingElement = <span>Loading...</span>;
 
-    return (
-      <div>
-        <span>
-          {storedJokes.map(({ id, joke }) => (<p key={id}>{joke}</p>))}
-        </span>
+//     return (
+//       <div>
+//         <span>
+//           {storedJokes.map(({ id, joke }) => (<p key={id}>{joke}</p>))}
+//         </span>
 
-      <p>
-        {
-          loading 
-            ? loadingElement
-            : <Joke jokeObj={jokeObj} saveJoke={this.saveJoke} />
-        }
-      </p>
+//       <p>
+//         {
+//           loading 
+//             ? loadingElement
+//             : <Joke jokeObj={jokeObj} saveJoke={this.saveJoke} />
+//         }
+//       </p>
 
-      </div>
-    );
-  }
-}
+//       </div>
+//     );
+//   }
+// }
 
-export default DadJoke;
+// export default DadJoke;
