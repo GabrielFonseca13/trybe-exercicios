887c4b485860a8bb768ec271301f07bbce913aaa
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-10-09 14:15:33 -0300

    revisao 2.2 renderização de listas

--- Front-End/Sessão 2 - Introdução ao React/lecture-2.2 - Componentes React/course2-2/src/components/Image.js
+++ Front-End/Sessão 2 - Introdução ao React/lecture-2.2 - Componentes React/course2-2/src/components/Image.js
@@ -1,11 +1,27 @@
+// // EX1 PROPS ##############
+// import React from "react";
+
+// class Image extends React.Component{
+//   render() {
+//     const { source, alt } = this.props;
+//     return(
+//       <div>
+//         <img src={source} alt={alt} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default Image;
+
 import React from "react";
 
 class Image extends React.Component{
   render() {
-    const { source, alt } = this.props;
+    const { source, altText } = this.props;
     return(
       <div>
-        <img src={source} alt={alt} />
+        <img src={source} alt={altText} />
       </div>
     );
   }
