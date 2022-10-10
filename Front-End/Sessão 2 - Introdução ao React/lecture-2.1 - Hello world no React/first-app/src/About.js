5ae3bae2001623ed61d25283b30cc76362a1b5be
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-10-10 16:04:02 -0300

    zerando o git

---
+++ Front-End/Sessão 2 - Introdução ao React/lecture-2.1 - Hello world no React/first-app/src/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class About extends React.Component{
+  render() {
+    return(
+      <div>
+        <h1>Gabriel Fonseca</h1>
+        <p>Trybe Student on T25 B</p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JAVASCRIPT</li>
+          <li>JEST</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default About;
