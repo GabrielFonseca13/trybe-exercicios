71c9204e18b167dce135ea62f21f9e8ac3202df0
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-10-31 22:54:19 -0300

    novos testes em requisição de api através do click no botao

--- Front-End/Sessão 6 - RTL React Testing Library/Dia 6.2 - RTL Mocks e Inputs/joke/src/App.test.js
+++ Front-End/Sessão 6 - RTL React Testing Library/Dia 6.2 - RTL Mocks e Inputs/joke/src/App.test.js
@@ -1,5 +1,7 @@
 // // App.test.js
 
+
+
 // ## Primeira forma de testar a requisição da API
 
 // import React from 'react';
@@ -25,16 +27,21 @@
 //   expect(renderedJoke).toBeInTheDocument();
 //   expect(global.fetch).toHaveBeenCalledTimes(1);
 //   expect(global.fetch).toHaveBeenCalledWith(
-//     'https://icanhazdadjoke.com/',
+  //     'https://icanhazdadjoke.com/',
 //     { headers: { Accept: 'application/json' } },
 //   );
 // });
 
 // ## Segunda forma de testar a requisição da API.
 
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
 afterEach(() => jest.clearAllMocks());
 
@@ -55,4 +62,43 @@ it('fetches a joke', async () => {
   expect(renderedJoke).toBeInTheDocument();
   expect(global.fetch).toHaveBeenCalledTimes(1);
   expect(global.fetch).toHaveBeenCalledWith('https://icanhazdadjoke.com/', { headers: { Accept: 'application/json' } });
+});
+
+
+
+it('fetches a new Joke when button is clicked', async () => {
+ const joke1 = {
+  id: '7h3oGtrOfxc',
+  joke: 'Whiteboards ... are remarkable.',
+  status: 200,
+ }; 
+
+
+ const joke2 = {
+  id: 'xXSv492wPmb',
+  joke: 'What is red and smells like blue paint? Red paint!',
+  status: 200,
+};
+
+ jest.spyOn(global, 'fetch');
+ global.fetch.mockResolvedValueOnce({
+  json: jest.fn().mockResolvedValue(joke1),
+ });
+
+ render(<App />);
+ const newJokeButton = screen.getByRole('button', {name: 'New Joke'});
+
+ expect(await screen.findByText(joke1.joke)).toBeInTheDocument();
+ expect(screen.queryByText(joke2.joke)).not.toBeInTheDocument();
+ expect(global.fetch).toBeCalledTimes(1);
+
+ global.fetch.mockResolvedValueOnce({
+  json: jest.fn().mockResolvedValue(joke2),
+ });
+
+ userEvent.click(newJokeButton);
+
+  expect(await screen.findByText(joke2.joke)).toBeInTheDocument();
+  expect(screen.queryByText(joke1.joke)).not.toBeInTheDocument();
+  expect(global.fetch).toBeCalledTimes(2);
 });
