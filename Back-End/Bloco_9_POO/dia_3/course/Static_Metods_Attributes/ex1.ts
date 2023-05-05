781e440bfd251a229a4252cec672893538563c65
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-05-05 15:16:47 -0300

    Refatorando exercicio anterior para utilizar o metodos estatico

---
+++ Back-End/Bloco_9_POO/dia_3/course/Static_Metods_Attributes/ex1.ts
@@ -0,0 +1,49 @@
+abstract class Character2 {
+  abstract talk(): void;
+  abstract specialMove(): void;
+
+  static characterPresentation(character: Character2): void {
+    character.talk();
+    character.specialMove();
+  }
+};
+
+class MelleCharacter2 extends Character2 {
+  constructor(private _name: string, private _specialMove: string) {
+    super();
+  }
+
+  talk(): void {
+    console.log(`Hi I'm ${this._name} i cant attack from a long reange.`);
+  }
+
+  specialMove(): void {
+    console.log(`${this._name} is using ${this._specialMove} !!!`)
+  }
+}
+
+class LongRangeCharacter2 extends Character2 {
+  constructor(private _name: string, private _specialMove: string) {
+    super();
+  }
+
+  talk(): void {
+    console.log(`Hi I'm ${this._name} i can attack from a long reange.`);
+  }
+
+  specialMove(): void {
+    console.log(`${this._name} is using ${this._specialMove} !!!`)
+  }
+}
+
+const yoshi2 = new MelleCharacter2('Yoshi', 'Super Dragon');
+const samus2 = new LongRangeCharacter2('Samus', 'Zero Laser');
+
+// const chars = (char: Character2) => {
+//   char.talk();
+//   char.specialMove();
+// }
+
+Character2.characterPresentation(yoshi2);
+Character2.characterPresentation(samus2);
+
