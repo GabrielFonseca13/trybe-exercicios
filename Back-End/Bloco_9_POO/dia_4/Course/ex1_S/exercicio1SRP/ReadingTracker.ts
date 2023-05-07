2fb20d1b661a80f22160921c0249718a7612e20e
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-05-07 13:50:30 -0300

    Implementando o Single Responsability Principle em Typescript

---
+++ Back-End/Bloco_9_POO/dia_4/Course/ex1_S/exercicio1SRP/ReadingTracker.ts
@@ -0,0 +1,28 @@
+import progressNotification from './notifications';
+
+class ReadingTracker {
+  private readingGoal: number;
+  private booksRead: number;
+
+  constructor(readingGoal: number) {
+    this.readingGoal = readingGoal;
+    this.booksRead = 0;
+  }
+
+  trackReadings(readsCount: number): void {
+    this.booksRead += readsCount;
+    if (this.booksRead >= this.readingGoal) {
+      progressNotification(
+        'COngratulations! You have reached your reading goal!',
+      );
+      return;
+    }
+    progressNotification(
+      'there are still some books to go!',
+    );
+  }
+}
+
+const readTracker = new ReadingTracker(20);
+readTracker.trackReadings(12);
+readTracker.trackReadings(9);
