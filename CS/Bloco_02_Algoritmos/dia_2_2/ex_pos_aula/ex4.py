9bc9e9dd181909c7b5ef7a0b11d45dcbffcab892
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-06-09 19:16:33 -0300

    achando o mdc de dois numeros com recursividade

---
+++ CS/Bloco_02_Algoritmos/dia_2_2/ex_pos_aula/ex4.py
@@ -0,0 +1,12 @@
+# 🚀 Exercício 4:
+# Escreva um algoritmo recursivo para encontrar o máximo divisor comum (mdc)
+# de dois inteiros.
+
+
+def mdc(a, b):
+    if b == 0:
+        return a
+    return mdc(b, a % b)
+
+
+print(mdc(5, 8))
