cc31f626e23c0072ded80804ad226e5c9521485b
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-06-21 15:01:10 -0300

    Iniciando POO em python

---
+++ CS/Bloco_03_Estrutura_de_dados1/dia01/course/ex2_classe_liquidificar.py
@@ -0,0 +1,43 @@
+class Liquidificador:
+    def __init__(self, cor, potencia, tensao, preco):
+        self.preco = preco
+        self.cor = cor
+        self._potencia = potencia
+        self._tensao = tensao
+        self.__ligado = False
+        self.__velocidade = 0
+        self.__velocidade_maxima = 3
+        self.__corrente_atual_no_motor = 0
+
+    def ligar(self, velocidade):
+        if velocidade > self.__velocidade_maxima or velocidade < 0:
+            raise ValueError(
+                f"Velocidade deve estar entre 0 e {self.__velocidade_maxima}"
+            )
+
+        self.__velocidade = velocidade
+        self.__corrente_atual_no_motor = (
+            (self._potencia / self._tensao) / self.__velocidade_maxima
+        ) * velocidade
+        self.__ligado = True
+
+    def desligar(self):
+        self.__ligado = False
+        self.__velocidade = 0
+
+    def esta_ligado(self):
+        return self.__ligado
+
+
+meu_liquidificador = Liquidificador("Azul", 200, 127, 200)
+seu_liquidificador = Liquidificador(
+    cor="Vermelho", potencia=250, tensao=220, preco=100
+)
+
+liquidificador_vermelho = Liquidificador("Vermelho", 250, 220, 100)
+liquidificador_vermelho.ligar(1)
+print("Está ligado?", liquidificador_vermelho.esta_ligado())
+# Está ligado? True
+liquidificador_vermelho.desligar()
+print("Está ligado?", liquidificador_vermelho.esta_ligado())
+# Está ligado? False
