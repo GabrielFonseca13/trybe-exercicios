6253abc46c6b312407f11686bc7ed79978b1e91b
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-06-23 10:21:57 -0300

    exemplos de poo em python

---
+++ CS/Bloco_03_Estrutura_de_dados1/dia01/course/ex4_composicao.py
@@ -0,0 +1,10 @@
+class Pessoa:
+    def __init__(self, nome, saldo_na_conta):
+        self.nome = nome
+        self.saldo_na_conta = saldo_na_conta
+        self.liquidificador = None
+
+    def comprar_liquidificador(self, liquidificador):
+        if liquidificador.preco <= self.saldo_na_conta:
+            self.saldo_na_conta -= liquidificador.preco
+            self.liquidificador = liquidificador
