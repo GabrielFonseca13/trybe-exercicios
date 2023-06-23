ebf6a4189c84276e49f2612d009d8a5e27356577
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-06-23 10:56:05 -0300

    implementando herança em puython

---
+++ CS/Bloco_03_Estrutura_de_dados1/dia01/course/ex6_super.py
@@ -0,0 +1,17 @@
+class A:
+    def faz_algo(self, valor):
+        print(valor)
+
+
+class B(A):
+    def faz_outra_coisa(self):
+        print("Vou printar o valor pelo método criado em A")
+        # Chama o método da classe A, que neste caso é a superclasse, passando
+        # o `self` explicitamente
+        A.faz_algo(self, valor=1)
+
+
+b = B()
+b.faz_outra_coisa()
+# Vou printar o valor pelo método criado em A
+# 1
