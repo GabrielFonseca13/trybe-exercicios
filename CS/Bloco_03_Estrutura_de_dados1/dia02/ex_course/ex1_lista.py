05b1d1b78841c7a79dbab3f6e50f23efb26671e6
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-06-25 14:30:03 -0300

    array multidimensional

--- CS/Bloco_03_Estrutura_de_dados1/dia02/ex_course/ex1_lista.py
+++ CS/Bloco_03_Estrutura_de_dados1/dia02/ex_course/ex1_lista.py
@@ -1,6 +1,7 @@
 """Perceba que temos uma coleção de valores
 e operações que atuam sobre estes valores,
 de acordo com o que foi definido pelo TAD."""
+import sys
 
 
 class ListaArray:
@@ -15,6 +16,13 @@ class ListaArray:
     def __str__(self):
         # converte para string e exibe os valores de data
         return str(self.data)
+
+    def remove(self, index):
+        # removeremos o item, retornando-o
+        return self.data.pop(index)
+
+    def update(self, index, value):
+        self.value[index] = value
 
     def get(self, index):
         # recupera o elemento no índice informado
@@ -44,3 +52,49 @@ while index < len(array):
     # recupera o elemento através de um índice
     print("Index:", index, ", Nome:", array.get(index))
     index += 1
+
+# sys.getsizeof retorna o tamanho da lista em bytes
+array_memory_size = sys.getsizeof(array.data)
+print(array_memory_size)
+
+# ...
+
+array.set(0, "Marcos")
+array.set(1, "Patrícia")
+# quando começamos as inserções o valor muda
+array_memory_size = sys.getsizeof(array.data)
+print(array_memory_size)  # 56
+
+array.set(2, "Matheus")
+array.set(3, "Giovana")
+# como um espaço adicional é reservado o valor não é modificado
+array_memory_size = sys.getsizeof(array.data)
+print(array_memory_size)  # 88
+
+array.set(4, "Alberto")
+array.set(5, "Marta")
+array.set(6, "Túlio")
+array.set(7, "Michelle")
+array_memory_size = sys.getsizeof(array.data)
+print(array_memory_size)  # 120
+
+
+array.set(0, "Marcos")
+array.set(1, "Patrícia")
+# print(array), internamente chama o método array.__str__() que implementamos
+print(array)  # saída: ["Marcos", "Patrícia"]
+
+# inserindo no começo do array
+array.set(0, "Valeria")
+print(array)  # saída: ["Valeria", "Marcos", "Patrícia"]
+
+# inserindo em uma posição intermediária
+array.set(1, "Miguel")
+print(array)  # saída: ['Valeria', 'Miguel', 'Marcos', 'Patrícia']
+
+array.set(0, "Marcos")
+array.set(1, "Patrícia")
+print(array)  # saída: ['Marcos', 'Patrícia']
+
+array.remove(0)  # retorna a string "Marcos"
+print(array)  # saída: ['Patrícia']
