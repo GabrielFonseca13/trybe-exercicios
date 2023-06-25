0aff19469a6df719f0b2b98cb3ba92223e1167c2
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-06-25 15:45:58 -0300

    iniciando exercicios dia 3.2

---
+++ CS/Bloco_03_Estrutura_de_dados1/dia02/ex_pos_aula/ex1.py
@@ -0,0 +1,17 @@
+# 🚀Exercício 1
+# Em um software monitor, o qual verifica a resiliência de outro
+# software, precisamos saber o tempo máximo que um software
+# permaneceu sem instabilidades. Para isto, a cada hora é feito
+# uma requisição ao sistema para verificamos se está tudo bem.
+# Supondo um array que contenha os estados coletados por nosso
+# software, calcule quanto tempo máximo que o servidor permaneceu
+# sem instabilidades.
+
+# 1 - OK
+# 0 - Instabilidades
+
+valores_coletados = [0, 1, 1, 1, 0, 0, 1, 1]
+resultado = 3
+
+valores_coletados = [1, 1, 1, 1, 0, 0, 1, 1]
+resultado = 4
