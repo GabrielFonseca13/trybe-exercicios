8cfdd090dbd3a56e10e6876c3b507b97c075186a
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-06-28 13:12:46 -0300

    exercicio 3 dia 3,3

--- CS/Bloco_03_Estrutura_de_dados1/dia02/ex_pos_aula/ex2.py
+++ CS/Bloco_03_Estrutura_de_dados1/dia02/ex_pos_aula/ex2.py
@@ -21,3 +21,7 @@ def shuffle(items):
     for offset in range(div_cards_by_two):
         answer.extend(items[offset::div_cards_by_two])
     return answer
+
+
+print(shuffle([2, 6, 4, 5]))
+print(shuffle([1, 4, 4, 7, 6, 6]))
