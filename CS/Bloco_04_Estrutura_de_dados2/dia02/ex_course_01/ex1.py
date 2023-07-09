250a81765f3552b199892c946d5aaf6c8f711bcc
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-07-09 20:34:20 -0300

    conjuntos

--- CS/Bloco_04_Estrutura_de_dados2/dia02/ex_course_01/ex1.py
+++ CS/Bloco_04_Estrutura_de_dados2/dia02/ex_course_01/ex1.py
@@ -20,9 +20,98 @@ class Conjunto:
         if item > self.last_element:
             self.last_element = item
 
+    def __str__(self):
+        string = "{"
 
+        for index, is_in_set in enumerate(self.set):
+            if is_in_set:
+                string += str(index)
+                if index < self.last_element:
+                    string += ", "
+
+        string += "}"
+        return string
+
+    def __contains__(self, item):
+        return self.set[item]
+
+    def union(self, conjunto_b):
+        new_conjunto = Conjunto()
+
+        for index in range(1001):
+            if self.set[index] or conjunto_b.set[index]:
+                new_conjunto.add(index)
+
+        return new_conjunto
+
+    def intersection(self, conjunto_b):
+        new_conjunto = Conjunto()
+
+        for index in range(1001):
+            if self.set[index] and conjunto_b.set[index]:
+                new_conjunto.add(index)
+
+        return new_conjunto
+
+
+# 1
+# if __name__ == "__main__":
+#     conj = Conjunto()
+
+#     for item in [0, 10, 100, 1000]:
+#         conj.add(item)
+
+# 2
+# if __name__ == "__main__":
+#     conj = Conjunto()
+#     for i in [0, 10, 100, 1000]:
+#         conj.add(i)
+#     print(conj)
+
+#     conj2 = Conjunto()
+#     for i in [1, 2, 3]:
+#         conj2.add(i)
+#     print(conj2)
+
+#     conj3 = Conjunto()
+#     for i in [7, 2, 10]:
+#         conj3.add(i)
+#     print(conj3)
+
+#     conj4 = Conjunto()
+#     print(conj4)
+
+# 3
+# if __name__ == "__main__":
+#     conj = Conjunto()
+#     for i in [1, 2, 3]:
+#         conj.add(i)
+#     print(conj)
+#     print(1 in conj)
+#     print(0 in conj)
+
+# 4
+# if __name__ == "__main__":
+#     conj1 = Conjunto()
+#     for i in range(1, 11):
+#         conj1.add(i)
+
+#     conj2 = Conjunto()
+#     for i in range(10, 21):
+#         conj2.add(i)
+
+#     conj3 = conj1.union(conj2)
+#     print(conj3)
+
+# 5
 if __name__ == "__main__":
-    conj = Conjunto()
+    conj1 = Conjunto()
+    for i in [1, 2, 3]:
+        conj1.add(i)
 
-    for item in [0, 10, 100, 1000]:
-        conj.add(item)
+    conj2 = Conjunto()
+    for i in [7, 2, 10]:
+        conj2.add(i)
+
+    conj3 = conj1.intersection(conj2)
+    print(conj3)
