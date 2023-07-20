f7b8e2079469a48f867e224df718b63bcfd391eb
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-07-19 21:13:16 -0300

    c# cast explicito

--- C#/sessao1/dia2/dia2/Program.cs
+++ C#/sessao1/dia2/dia2/Program.cs
@@ -1,10 +1,24 @@
-﻿public class Program
+﻿// CAST IMPLICITO
+
+// public class Program
+// {
+//   public static void Main()
+//   {
+//     int someIntNumber = 51;
+//     long longNumberCast = someIntNumber;
+
+//     Console.WriteLine(longNumberCast);
+//   }
+// }
+
+// cast EXPLICITO
+public class Program
 {
   public static void Main()
   {
-    int someIntNumber = 51;
-    long longNumberCast = someIntNumber;
+    long someLongNumber = 516144564564654;
+    int intNumber = (int)someLongNumber;
 
-    Console.WriteLine(longNumberCast);
+    Console.WriteLine(intNumber);
   }
 }
