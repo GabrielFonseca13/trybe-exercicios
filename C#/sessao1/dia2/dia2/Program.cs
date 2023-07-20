9161f09a9b84ee4ea445a2ec906e321025306850
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-07-19 21:23:37 -0300

    c# switch case

--- C#/sessao1/dia2/dia2/Program.cs
+++ C#/sessao1/dia2/dia2/Program.cs
@@ -36,26 +36,43 @@
 // }
 
 // ESTRUTURAS DE CONTROLE IF, ELSE, SWITCH E CASE
-public class Program
+// public class Program
+// {
+//   public static void Main()
+//   {
+//     Console.WriteLine("Informe o raio de um círculo (deve ser um número inteiro)");
+//     string? response = Console.ReadLine();
+//     int radius = 0;
+//     var canConvert = Int32.TryParse(response, out radius);
+
+//     if (canConvert)
+//     {
+//       const double pi = 3.14159;
+
+//       double circumference = pi * (2 * radius);
+
+//       Console.WriteLine("A circunferência de um circulo com raio " + radius + " é igual a " + circumference);
+//     }
+//     else
+//     {
+//       Console.WriteLine("O texto digitado não é um número inteiro.");
+//     }
+//   }
+// }
+
+// EXEMPLO SWITCH CASE
+
+var number = 0;
+
+switch (number)
 {
-  public static void Main()
-  {
-    Console.WriteLine("Informe o raio de um círculo (deve ser um número inteiro)");
-    string? response = Console.ReadLine();
-    int radius = 0;
-    var canConvert = Int32.TryParse(response, out radius);
-
-    if (canConvert)
-    {
-      const double pi = 3.14159;
-
-      double circumference = pi * (2 * radius);
-
-      Console.WriteLine("A circunferência de um circulo com raio " + radius + " é igual a " + circumference);
-    }
-    else
-    {
-      Console.WriteLine("O texto digitado não é um número inteiro.");
-    }
-  }
+  case > 0:
+    Console.WriteLine("maior que 0");
+    break;
+  case 0:
+    Console.WriteLine("igual a zero");
+    break;
+  default:
+    Console.WriteLine("menor que zero");
+    break;
 }
