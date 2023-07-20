526cf97cbf38476c27857d62541d76c9a2ca077e
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-07-19 21:18:36 -0300

    c# Estruturas de controle if, else, switch case

--- C#/sessao1/dia2/dia2/Program.cs
+++ C#/sessao1/dia2/dia2/Program.cs
@@ -24,13 +24,38 @@
 // }
 
 // CONVERTENDO STRING PARA NUMEROS
+// public class Program
+// {
+//   public static void Main()
+//   {
+//     string someString = "42";
+//     int convertInt = Convert.ToInt32(someString);
+
+//     Console.WriteLine(convertInt);
+//   }
+// }
+
+// ESTRUTURAS DE CONTROLE IF, ELSE, SWITCH E CASE
 public class Program
 {
   public static void Main()
   {
-    string someString = "42";
-    int convertInt = Convert.ToInt32(someString);
+    Console.WriteLine("Informe o raio de um círculo (deve ser um número inteiro)");
+    string? response = Console.ReadLine();
+    int radius = 0;
+    var canConvert = Int32.TryParse(response, out radius);
 
-    Console.WriteLine(convertInt);
+    if (canConvert)
+    {
+      const double pi = 3.14159;
+
+      double circumference = pi * (2 * radius);
+
+      Console.WriteLine("A circunferência de um circulo com raio " + radius + " é igual a " + circumference);
+    }
+    else
+    {
+      Console.WriteLine("O texto digitado não é um número inteiro.");
+    }
   }
 }
